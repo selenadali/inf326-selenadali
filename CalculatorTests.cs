3955486600edbe00523df35b1272f995fb2cb507
Selen ADALI <selenadali94@gmail.com>
2015-10-21 01:49:18 +0300

    PluralSight - Introduction to .NET Testing with NUnit
    First Module - SimpleCalculator and CalculatorTests

---
+++ CalculatorTests.cs
@@ -0,0 +1,31 @@
+﻿
+using NUnit.Framework;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class SimpleCalculatorTests
+    {
+        [Test]
+        public void ShoulAddTwoNumbers()
+        {
+            var sut = new SimpleCalculator();
+
+            var result = sut.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+
+        }
+        [Test]
+        public void ShoulMultiplyTwoNumbers()
+        {
+            var sut = new SimpleCalculator();
+
+            var result = sut.Multiply(2, 10);
+
+            Assert.That(result, Is.EqualTo(20));
+
+        }
+    }
+}
+
---
+++ SimleCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class SimpleCalculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Multiply(int a, int b)
+        {
+            //Bug for demo purposes
+            return a * b;
+        }
+    }
+}
